6cd13b9bf22699fac937d7137a867c7e9fcc39e5
Tribhuwan Singh <tribhuwan50@gmail.com>
2020-06-22 20:00:02 -0700

    commit 6

---
+++ Assets/Scenes/Scripts/PlayerStone.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerStone : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        theDiceRoller = GameObject.FindObjectOfType<DiceRoller>();
+    }
+
+    DiceRoller theDiceRoller;
+    
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnMouseUp()
+    {
+        Debug.Log("Click!");
+
+        // Have we rolled the Dice
+        if(!theDiceRoller.IsDoneRolling)
+        {
+            // Don't move the stone yet
+            return;
+        }
+        
+    }
+    
+}
